a4e50c713c9215e539a321dbfc7c2a1c637d1a80
unknown <omobowalerohma1998@gmail.com>
2023-08-17 22:15:25 +0100

    finally did the join method

--- mysqlite/my_sqlite_request.rb
+++ mysqlite/my_sqlite_request.rb
@@ -1,6 +1,6 @@
 require "csv"
 class MySqliteRequest
-attr_accessor :table_name, :table_data,:hashedDataB, :request, :columns ,:result_hash_array,:isWhere,:where_count,:isJoin,:column_on_db_a,:column_on_db_b,:filter_column, :filter_column_value
+attr_accessor :table_name, :hashedDataA,:hashedDataB, :request,:filename_db_b, :columns ,:selected_hash_array,:isWhere,:where_count,:isJoin,:column_on_db_a,:column_on_db_b,:filter_column, :filter_column_value
 def initialize
 @isWhere=false
 @isJoin=false
@@ -31,9 +31,10 @@ end
 def join(column_on_db_a, filename_db_b, column_on_db_b)
     # Read filename_db_b table data
     @isJoin=true
-    @hashedDataB = CSV.parse(File.read(filename_db_b), headers: true).map(&:to_h).take(200)
+   
     @column_on_db_a=column_on_db_a
     @column_on_db_b=column_on_db_b
+    @filename_db_b=filename_db_b
    
     self
 end
@@ -41,21 +42,36 @@ end
   
 
 def run
-    @result_hash_array=[] 
-    @joined_hash_array=[]
+    @hashedDataA=table_to_hashed(@table_name)
+    @selected_hash_array=[] 
     @filtered_hash_array=[]
+    @merged_hash_array=[]
     @final=[]
     
-        
+    
+    if @isJoin
+        @hashedDataB = CSV.parse(File.read(@filename_db_b), headers: true).map(&:to_h).take(5)    
+        @hashedDataA.each do |rowA|
+        result_hash={}
+            @hashedDataB.each do |rowB|
+                if rowB[@column_on_db_b] === rowA[@column_on_db_a]
+                merged = rowA.merge(rowB)
+                 @merged_hash_array<<merged
+                end
+            end
+        end#hashsedDataA
+        @hashedDataA=@merged_hash_array
+    end#is join
+
+       
     #convert table data in CSV::Row to a HashedData
     if @request==='select' # select the columns  from the each table row and corresponding data
-    @table_data=table_to_hashed(@table_name)
-        table_data.each do |current_row|
+        @hashedDataA.each do |current_row|
             result_hash={}
             @all_conditions_met=true;
 
-            process_row(current_row, result_hash, @result_hash_array, @columns)
-            @final=@result_hash_array
+            process_row(current_row, result_hash, @selected_hash_array, @columns)
+            @final=@selected_hash_array
         
 
             if @isWhere                
@@ -75,33 +91,24 @@ def run
         
  end #of request='select'
     
-    if @isJoin 
-        @result_hash_array.each do |rowA|
-        @hashedDataB.each do |rowB|
-          if rowB[@column_on_db_b] == rowA[@column_on_db_a]
-            merged = rowA.merge(rowB)
-            @joined_hash_array<<merged
-          end
-        end
-      end  
-       @final=@joined_hash_array
-    end     
-    puts ".......................final............"
-    puts @final.inspect 
+    
 
+
+puts '..........................final result'   
+puts @final.inspect
 end#of def run
 end#of class
 
 #HELPER FUNCTIONS
 
 def table_to_hashed(table_name)
-hashedData=CSV.parse(File.read(table_name),headers:true).map(&:to_h).take(55)
+hashedData=CSV.parse(File.read(table_name),headers:true).map(&:to_h).take(5)
 return hashedData
 end
 
-def process_row(row,result_hash, result_hash_array,columns)
+def process_row(row,result_hash, selected_hash_array,columns)
     if  columns[0]==='*'
-        result_hash_array<<row
+        selected_hash_array<<row
    
     else
         columns.each do |column_name| 
@@ -109,14 +116,18 @@ def process_row(row,result_hash, result_hash_array,columns)
 
         end
     end  
-    result_hash_array << result_hash if result_hash != {}
+    selected_hash_array << result_hash if result_hash != {}
+    
 end
 
 
 request = MySqliteRequest.new
 request = request.from('nba_player_data.csv')
-request = request.select('name',"height")
+request = request.select("name",'weight','weight2','college','year_start','player')
 request = request.where('college', 'University of California')
 request = request.where('year_start', '1997')
 # request =request.join('college','nba_players.csv','college')
+
 request.run
+
+ 
